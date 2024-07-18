c71ac647377db9dc490e090aeae86e1d21095231
karunpat.pro <karunpat.pro@gmail.com>
2024-07-18 23:46:57 +0700

    update: randomize password

--- randomize-password.sh
+++ randomize-password.sh
@@ -3,11 +3,16 @@
 echo "Starting generate password..."
 
 #Random password generation
-MYSQL_ROOT_PASSWORD=$(openssl rand -base64 64)
-MYSQL_PASSWORD=$(openssl rand -base64 64)
+MYSQL_ROOT_PASSWORD=$(openssl rand -hex 64)
+MYSQL_PASSWORD=$(openssl rand -hex 64)
 
 # Echo password to .env file
 echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
-MYSQL_PASSWORD=$MYSQL_PASSWORD" > .env
+MYSQL_PASSWORD=$MYSQL_PASSWORD
+MYSQL_DATABASE=db
+MYSQL_USER=user
+DOCKER_NETWORK=backend
+DB_IMAGE=mysql
+DB_VERSION=latest" > .env
 
 echo "Generate password success!"
